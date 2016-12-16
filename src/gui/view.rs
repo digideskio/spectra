2965311132e775812ca238bb30df67a79d7cb1f5
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-12-16 03:31:53 +0100

    Widget uniforms added.

--- src/gui/view.rs
+++ src/gui/view.rs
@@ -1,14 +1,18 @@
 use luminance::{Dim2, Flat, Mode, RGBA32F};
-use luminance_gl::gl33::{Framebuffer, Tess, Texture};
+use luminance_gl::gl33::{Framebuffer, Tess, Texture, Uniform};
 
 use id::Id;
 use gui::widget::FillRectWidget;
 use scene::Scene;
 use shader::Program;
 
+const WIDGET_VIEW_RESOLUTION: Uniform<[u32; 2]> = Uniform::new(0);
+const WIDGET_VIEW_WIDGET_RECT: Uniform<[i32; 4]> = Uniform::new(1);
+const WIDGET_VIEW_WIDGET_COLOR: Uniform<[f32; 3]> = Uniform::new(2);
+
 /// Default widget interpretor.
 pub struct WidgetView<'a> {
-  // available framebuffer for the all GUI
+  // available framebuffer for the whole GUI
   framebuffer: Framebuffer<Flat, Dim2, Texture<Flat, Dim2, RGBA32F>, ()>,
   // program used to render widgets
   program: Id<'a, Program>,
@@ -19,7 +23,11 @@ pub struct WidgetView<'a> {
 impl<'a> WidgetView<'a> {
   pub fn new(w: u32, h: u32, scene: &mut Scene<'a>) -> Self {
     let framebuffer = Framebuffer::new((w, h), 0).unwrap();
-    let program = scene.get_id("spectra/gui/default.glsl", vec![]).unwrap();
+    let program = scene.get_id("spectra/gui/default.glsl", vec![
+      Uniform::<[u32; 2]>::sem("resolution"),
+      Uniform::<[i32; 4]>::sem("widget_rect"),
+      Uniform::<[f32; 3]>::sem("widget_color")
+    ]).unwrap();
     let quad = Tess::attributeless(Mode::TriangleStrip, 4);
 
     WidgetView {
@@ -29,3 +37,4 @@ impl<'a> WidgetView<'a> {
     }
   }
 }
+
